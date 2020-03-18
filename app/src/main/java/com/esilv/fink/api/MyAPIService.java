33249b09cf5dc0f07a166dd61cc1a5120f7c90f1
SOUDRY Gabriel <gabriel.soudry@edu.devinci.fr>
2020-03-18 16:09:20 +0100

    change api call

--- app/src/main/java/com/esilv/fink/api/MyAPIService.java
+++ app/src/main/java/com/esilv/fink/api/MyAPIService.java
@@ -6,6 +6,6 @@ import retrofit2.http.Query;
 
 
 public interface MyAPIService {
-    @GET("/get/query=select%20*%20from%20user_table%20where%20CUSTOMERID%20>=%2016000000")
+    @GET("/get/?query=select%20*%20from%20user_table%20where%20CUSTOMERID%20>=%2016000000")
     Call<ApiGetResponse> search();
 }
